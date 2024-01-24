5a0523b998b91f28e5b97bf8c05fa51b590ae6c5
pavel augustine <123310036+voidaugust@users.noreply.github.com>
2024-01-24 18:09:39 +0100

    deploy set up try 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/demo-next-app' : '',
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://voidaugust.github.io/demo-next-app' : undefined,
 };
 
-export default nextConfig;
+export default nextConfig
